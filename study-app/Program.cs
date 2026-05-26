040197c60ff005b4d3009fa1efe866b5404e6f82
Alessandro <alessandropwhite@gmail.com>
2026-05-26 12:44:50 +0100

    Basic classes, nothing concrete

---
+++ study-app/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+class Topics
+{
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public int ID { get; set; }
+}
+
+class notes : Topics
+{
+    public string note { get; set; }
+}
+
+class account
+{
+    private string Username { get; set; }
+    private string Email { get; set; }
+    private string Password { get; set; }
+}
+
